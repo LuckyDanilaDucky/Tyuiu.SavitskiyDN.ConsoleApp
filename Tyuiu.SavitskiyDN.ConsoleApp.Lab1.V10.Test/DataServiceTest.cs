d1652f9b2a78566c3f08c4d7573292a6159a982f
LuckyDanilaDucky <danila_with_corona@vk.com>
2023-10-17 22:34:00 +0500

    lab 1

---
+++ Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Test/DataServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Lib;
+
+namespace Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpressionV_1_1()
+        {
+            double x = 5;
+            double y = 6;
+            double a = 9;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_1_1(x, y, a);
+
+            result = Math.Round(result, 6);
+            double wait = 534.387395;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_1_2()
+        {
+            double x = 4;
+            double y = -3;
+            double a = 7;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_1_2(x, y, a);
+
+            result = Math.Round(result, 8);
+            double wait = 19.78571429;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_5_1()
+        {
+            double x = 5;
+            double y = 1;
+            double a = -2;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_3_1(x, y, a);
+
+            result = Math.Round(result, 13);
+            double wait = -2.21429;
+
+            Assert.AreEqual(result, wait);
+
+        }
+
+        [TestMethod]
+        public void ValidExpressionV_5_2()
+        {
+            double x = 5;
+            double y = 1;
+            double a = -2;
+            DataService ds = new DataService();
+
+            double result = ds.SolveExpressV_3_2(x, y, a);
+
+            result = Math.Round(result, 12);
+            double wait = 16;
+
+            Assert.AreEqual(result, wait);
+
+        }
+    }
+}
---
+++ Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07241e39-2d9f-4061-b8e5-1b4419328eea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
