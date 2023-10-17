c98df1574e483c6128f203503a45d214736057ae
LuckyDanilaDucky <danila_with_corona@vk.com>
2023-10-17 22:34:35 +0500

    lab 1

---
+++ Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Lib
+{
+    public class DataService
+    {
+        public double SolveExpressV_1_1(double x, double y, double a)
+        {
+            double result = ((3 + x / y) / ((2 * a) / x)) - ((3 * x * a + 2 * a + a * y) / (3 * a + 2 * x - y)) + (10 * y * a);
+            return result;
+        }
+
+        public double SolveExpressV_1_2(double x, double y, double a)
+        {
+            double result = (3 * x) + ((3 * x + y - 4 * a) / (a + 2 * x + 7 * y)) + 5 + (((a / y) + 1) / ((2 * a) / (x)));
+            return result;
+        }
+
+        public double SolveExpressV_3_1(double x, double y, double a)
+        {
+            double result = 2 * y + ((2 * 3 * a) / (10 * x - 3 * a)) + 2 + (((a + 2) / y) / ((3 * a) / x)) + 3 * a;
+            return result;
+        }
+
+        public double SolveExpressV_3_2(double x, double y, double a)
+        {
+            double result = (3 * a / y) * y - ((3 * x * a + 2 * a + a * y) / (3 * a + 2 * x - y)) + 12 * y + ((a / y + 2) / (3 * a / x)) + y * a;
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10.Lib;
+
+namespace Tyuiu.SavitskiyDN.ConsoleApp.Lab1.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Лабораторная работа #1 | Выполнил: Савицкий Д.Н. | НТм-22-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Лабораторная работа #1                                                  *");
+            Console.WriteLine("* Расчёты арифметических и математических выражений в C#                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Савицкий Данила Николаевич | НТм-22-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на С#, которая выполняет расчёты          *");
+            Console.WriteLine("* арифметических и математических выражений.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x = 5;
+            double y = 1;
+            double a = -2;
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("a = " + a);
+
+            DataService ds = new DataService();
+
+            double result1 = ds.SolveExpressV_3_1(x, y, a);
+            double result2 = ds.SolveExpressV_3_2(x, y, a);
+
+            result1 = Math.Round(result1, 13);
+            result2 = Math.Round(result2, 12);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" 1-е выражение: " + result1);
+            Console.WriteLine(" 2-е выражение: " + result2);
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.ReadKey();
+        }
+    }
+}
