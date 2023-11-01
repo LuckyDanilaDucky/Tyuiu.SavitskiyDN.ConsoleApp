2bc044b4042c0f82dd4286138c168a80cef18f0a
LuckyDanilaDucky <danila_with_corona@vk.com>
2023-11-01 16:56:54 +0500

    Lab4 is done

---
+++ Tyuiu.SavitskiyDN.ConsoleApp.Lab4.V10.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tyuiu.SavitskiyDN.ConsoleApp.Lab4.V10.Lib
+{
+    public class DataService
+    {
+        public bool GetPointShape(double x, double y)
+        {
+            bool res;
+            if ((y <= -x && y >= Math.Pow(x, 2) - 2) || (y <= x && y >= Math.Pow(x, 2) - 2))
+
+            {
+                res = true;
+
+            }
+
+            else
+            {
+                res = false;
+            };
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SavitskiyDN.ConsoleApp.Lab4.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SavitskiyDN.ConsoleApp.Lab4.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SavitskiyDN.ConsoleApp.Lab4.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("20a3c489-9063-404a-887d-444b794d5a09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
